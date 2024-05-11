2b6fd9e541a85d05ea2161ba517efd0a2e993278
Tuyisenge Jordan <jordanale.112@gmail.com>
2024-05-11 18:41:07 +0200

    task 1 fix err 5

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 #Using Puppet, install flask from pip3
 class { 'flask':
-    package { 'python3-flask 2.1.0':
-        ensure   => 'installed',
+    package { 'python3-flask':
+        ensure   => '2.1.0',
         provider => 'pip3',
     }
     require => Package['pip3'],
