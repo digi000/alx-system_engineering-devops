2778e57cb6c33b4aa628293bfea0164cb8bb8fe4
Tuyisenge Jordan <jordanale.112@gmail.com>
2024-05-11 18:10:17 +0200

    task 1 reduce ine

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 #Using Puppet, install flask from pip3
 package { 'flask':
-    ensure          => 'installed',
+    ensure          => '2.1.0',
     provider        => 'pip3',
-    install_options => ['--version', '2.1.0'],
 }
