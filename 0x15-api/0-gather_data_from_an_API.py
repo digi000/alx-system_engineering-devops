5f8e9e31074024191ab6c771cb417c2fc0839f6b
Tuyisenge Jordan <jordanale.112@gmail.com>
2025-06-23 23:44:38 +0200

    0t1

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,16 +4,17 @@
 import requests
 import sys
 
-urId = sys.argv[1]
-user = requests.get(f'https://jsonplaceholder.typicode.com/users/{urId}')
-userDt = user.json()
-userName = userDt['name']
-tasks = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={urId}')
-tasksDt = tasks.json()
-tasksTotal = len(tasksDt)
-tasksCp = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={urId}&&completed=true')
-tasksCd = tasksCp.json()
-tasksCc = len(tasksCd)
-print(f"Employee {userName} is done with tasks({tasksCc}/{tasksTotal})")
-for task in tasksCd:
-    print(f"\t {task['title']}")
+if __name__ == "__main__":
+    urId = sys.argv[1]
+    user = requests.get(f'https://jsonplaceholder.typicode.com/users/{urId}')
+    userDt = user.json()
+    userName = userDt['name']
+    tasks = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={urId}')
+    tasksDt = tasks.json()
+    tasksTotal = len(tasksDt)
+    tasksCp = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={urId}&&completed=true')
+    tasksCd = tasksCp.json()
+    tasksCc = len(tasksCd)
+    print(f"Employee {userName} is done with tasks({tasksCc}/{tasksTotal})")
+    for task in tasksCd:
+        print(f"\t {task['title']}")
